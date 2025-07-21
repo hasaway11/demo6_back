63897596aca68d8b6860bff1ffe4fc96cc89f1ae
hasaway2 <hasaway2@naver.com>
2025-07-21 09:22:46 +0900

    최종 배포일까?

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.*;
 
+import io.swagger.v3.oas.annotations.*;
 import jakarta.servlet.http.*;
 import org.springframework.http.*;
 import org.springframework.security.core.*;
@@ -16,6 +17,7 @@ public class AuthController {
 	// 프론트엔드에서 현재 로그인 상태를 물어오면 응답 
 	// 로그인한 경우 : 200 + 로그인 아이디
 	// 비로그인 : 409 + null
+	@Operation(summary="로그인 여부", description="로그인 여부 확인-username, role로 응답")
 	@GetMapping(path="/api/auth/check")
 	public ResponseEntity<Map<String, String>> checkLogin(Authentication authentication) {
 		if(authentication!=null) {
@@ -26,6 +28,7 @@ public class AuthController {
 		return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
 	}
 
+	@Operation(summary="비밀번호 확인을 했는 지", description="비밀번호 확인 여부(세션을 사용하므로 로그인 필요)")
 	@GetMapping(path="/api/auth/check-password")
 	public ResponseEntity<Void> isCheckPassword(HttpSession session) {
 		if(session.getAttribute("checkPassword") != null)
